d9668848b2f493d7e2ccc1857afd8b51f3be2387
arteptiber <arteptiber@gmail.com>
2021-08-20 19:28:47 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,25 @@
+module github.com/arteptiber/techchart
+
+go 1.15
+
+require (
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/btcsuite/btcd v0.22.0-beta // indirect
+	github.com/deckarep/golang-set v1.7.1 // indirect
+	github.com/ethereum/go-ethereum v1.10.7
+	github.com/gballet/go-libpcsclite v0.0.0-20191108122812-4678299bea08 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/jackpal/go-nat-pmp v1.0.2 // indirect
+	github.com/joho/godotenv v1.3.0
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/prometheus/tsdb v0.10.0 // indirect
+	github.com/rjeczalik/notify v0.9.2 // indirect
+	github.com/shirou/gopsutil v3.21.7+incompatible // indirect
+	github.com/status-im/keycard-go v0.0.0-20200402102358-957c09536969 // indirect
+	github.com/tklauser/go-sysconf v0.3.7 // indirect
+	github.com/tyler-smith/go-bip39 v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20210813211128-0a44fdfbc16e // indirect
+	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/big"
+	"os"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/joho/godotenv"
+
+	factory "github.com/arteptiber/techchart/contracts/factory"
+	pool "github.com/arteptiber/techchart/contracts/pool"
+	token "github.com/arteptiber/techchart/contracts/token"
+)
+
+var tokens map[string]common.Address = map[string]common.Address{
+	"WETH": common.HexToAddress("0xc778417e063141139fce010982780140aa0cd5ab"),
+	"UNI":  common.HexToAddress("0x1f9840a85d5af5bf1d1762f925bdaddc4201f984"),
+}
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Panic(err)
+	}
+}
+
+func main() {
+	if providerURL, exists := os.LookupEnv("PROVIDER_URL"); exists {
+		connect(providerURL)
+	}
+}
+
+func connect(providerURL string) {
+	client, err := ethclient.Dial(providerURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	address := common.HexToAddress("0x1F98431c8aD98523631AE4a59f267346ea31F984")
+	instance, err := factory.NewFactory(address, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fee := &big.Int{}
+	fee.SetInt64(10000)
+
+	poolAddress, err := instance.GetPool(nil, tokens["WETH"], tokens["UNI"], fee)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	poolInstance, err := pool.NewPool(poolAddress, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_ = poolInstance
+
+	token0Instance, err := token.NewToken(tokens["WETH"], client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	token1Instance, err := token.NewToken(tokens["UNI"], client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(token0Instance.BalanceOf(nil, poolAddress))
+	fmt.Println(token1Instance.BalanceOf(nil, poolAddress))
+}
